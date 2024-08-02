fe246f73d93eae5eb5a33eec2b908f3dd9154bf6
John <j.gaji@meneses.law>
2024-08-02 11:13:54 -0500

    tasks

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,5 +33,5 @@ CREATE TABLE Order_Details(
     order_detail_id INT PRIMARY KEY,
     quantity DOUBLE,
     order_id INT FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    book_id FOREIGN KEY(book_id) REFERENCES Books (book_id)
+    book_id INT FOREIGN KEY(book_id) REFERENCES Books (book_id)
 );
