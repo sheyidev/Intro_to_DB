27dd6a206988f77dc87e1458508fde091fc8ec39
John <j.gaji@meneses.law>
2024-08-02 10:53:02 -0500

    tasks

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,5 @@
 
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
@@ -9,6 +9,16 @@ CREATE TABLE Books (
     price DOUBLE,
     publication_date DATE
     author_id Foreign Key (author_id) REFERENCES Authors (author_id)
+);
 
-)
+CREATE TABLE Authors (
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215)
+);
 
+CREATE TABLE Customers(
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
